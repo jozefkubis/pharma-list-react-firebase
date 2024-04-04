c6587921bab6948f2de410293fad4a84a0cacf00
Jozef Kubis <kubis.jozef@outlook.com>
2024-04-04 23:11:57 +0200

    Nedokoneny dark-mode v navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,22 @@ import { NavLink } from "react-router-dom"
 import { HiHome } from "react-icons/hi2"
 import { IoBagAdd } from "react-icons/io5"
 import { AiFillDelete } from "react-icons/ai"
+import { useEffect } from "react"
 
 const Navbar = () => {
+  const darkMode = () => {
+    const isDarkMode = document.body.classList.toggle("dark-mode")
+    const isDarkModeH = document.querySelector("header").classList.toggle("dark-mode")
+    localStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled")
+    localStorage.setItem("darkModeH", isDarkModeH ? "enabled" : "disabled")
+  }
 
-const darkMode = () => {
-  document.body.classList.toggle("dark-mode")
-}
+  useEffect(() => {
+    const darkModeSetting = localStorage.getItem("darkMode")
+    if (darkModeSetting === "enabled") {
+      document.body.classList.add("dark-mode")
+    }
+  }, [])
 
   return (
     <header>
@@ -22,7 +32,9 @@ const darkMode = () => {
         <NavLink to="/delete">
           <AiFillDelete /> Zmazať liek
         </NavLink>
-        <button className="navbar-btn" onClick={darkMode}>Tmavy rezim</button>
+        <button className="navbar-btn" onClick={darkMode}>
+          Tmavy rezim
+        </button>
       </nav>
     </header>
   )
